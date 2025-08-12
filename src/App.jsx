acf1f0d870c50c86a7369f76c0dce4af4f9c5e8a
Francesco Ruocco <francyruocco97@gmail.com>
2025-08-12 12:13:18 +0200

    Aggiornamento di alcuni file

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import GalactusImage from './components/Image';
 function App() {
   return (
     <div className="relative h-screen w-full bg-cover bg-center bg-no-repeat overflow-hidden flex flex-col items-center justify-center px-4" style={{ backgroundImage: `url(${sfondo})` }}>
-      <GalactusText className="font-extrabold leading-none text-center text-[80px] md:text-[220px] max-w-full break-words overflow-hidden" />
-      <GalactusImage className="max-w-full max-h-[40vh] md:max-h-[70vh] object-contain mt-4 md:mt-0" />
+      <GalactusText/>
+      <GalactusImage/>
     </div>
   );
 }
